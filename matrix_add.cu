331b68c91beb3b77b85b0c16c1c862e5c76a6926
Kelvin <kelvinkonoor@gmail.com>
2025-01-19 13:50:44 +0530

    cuda memory allocation

--- matrix_add.cu
+++ matrix_add.cu
@@ -6,18 +6,26 @@
 int main() {
     srand(time(NULL));
     int M = 128, N = 128;
+    int size = M * N;
 
-    int *h_matA = (int*)malloc(M * N * sizeof(int));
-    int *h_matB = (int*)malloc(M * N * sizeof(int));
+    int *h_matA = (int*)malloc(size * sizeof(int));
+    int *h_matB = (int*)malloc(size * sizeof(int));
+    int *h_matC = (int*)malloc(size * sizeof(int));
 
     for(int i = 0;i < M * N; i++) {
         h_matA[i] = rand() % 100;
         h_matB[i] = rand() % 100;
     }
 
+    int *d_matA, *d_matB, *d_matC;
+
+    cudaMalloc((void**)&d_matA, size * sizeof(int));
+    cudaMalloc((void**)&d_matB, size * sizeof(int));
+    cudaMalloc((void**)&d_matC, size * sizeof(int));
+
     printf("%d %d", h_matA[0], h_matB[0]);
     free(h_matA);
     free(h_matB);
-    
+
     return 0;
 }
