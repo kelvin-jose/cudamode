9ffdaa1093c94f29757ac5244a995df5100303c7
Kelvin <kelvinkonoor@gmail.com>
2025-02-07 12:40:59 +0530

    caling kernel and copying the results back to host memory

--- matrix_vector_mul.cu
+++ matrix_vector_mul.cu
@@ -5,18 +5,18 @@
 
 __global__ void matrix_vector_mult(int *matrix, int *vector, int *output, size_t N) {
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-
+    
     if (i < N) {
-        float sum = 0.0;
+        int sum = 0;
         for(int j = 0; j < N; j++)
-            sum += matrix[i * N + j] * vector[j];
+            sum += (matrix[i * N + j] * vector[j]);
         output[i] = sum;
     }
 }
 
 int main() {
     srand(time(NULL));
-    int N = 128;
+    int N = 2;
     int *h_matrix, *h_vector, *h_output, *d_matrix, *d_vector, *d_output;
     size_t matrix_size = sizeof(int) * N * N;
     size_t vector_size = sizeof(int) * N;
@@ -38,15 +38,28 @@ int main() {
     cudaMemcpy(d_matrix, h_matrix, matrix_size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_vector, h_vector, vector_size, cudaMemcpyHostToDevice);
 
-    dim3 block_size(32);
+    dim3 block_size(2);
     dim3 grid_size((N + block_size.x - 1) / block_size.x);
 
     matrix_vector_mult<<<grid_size, block_size>>>(d_matrix, d_vector, d_output, N);
+    cudaDeviceSynchronize();
 
-    printf("%d", h_matrix[10]);
+    cudaMemcpy(h_output, d_output, vector_size, cudaMemcpyDeviceToHost);
+
+    for(int j = 0; j < N * N; j++)
+        printf("%d\n", h_matrix[j]);
+
+    for(int j = 0; j < N; j++)
+        printf("%d\n", h_vector[j]);
+    
+    for(int i = 0; i < N; i++)
+        printf("%d\n", h_output[i]);
+    
     free(h_matrix);
     free(h_vector);
+    free(h_output);
     cudaFree(d_matrix);
     cudaFree(d_vector);
+    cudaFree(d_output);
     return 0;
 }
