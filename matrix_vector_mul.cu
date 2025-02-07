84931fd86545c9a1dcaeb5dfce7abde9e2f20ba0
Kelvin <kelvinkonoor@gmail.com>
2025-02-07 11:19:48 +0530

    generating and saving random integers in host memory

--- matrix_vector_mul.cu
+++ matrix_vector_mul.cu
@@ -4,5 +4,23 @@
 #include<cuda_runtime.h>
 
 int main() {
+    srand(time(NULL));
+    int N = 128;
+    int *h_matrix, *h_vector, *d_matrix, *d_vector;
+    size_t matrix_size = sizeof(int) * N * N;
+    size_t vector_size = sizeof(int) * N;
+
+    h_matrix = (int*)malloc(matrix_size);
+    h_vector = (int*)malloc(vector_size);
+
+    for(int i = 0; i < N * N; i++) 
+        h_matrix[i] = rand() % 10;
+    
+    for(int i = 0;i < N; i++)
+        h_vector[i] = rand() % 10;
+
+    printf("%d", h_matrix[10]);
+    free(h_matrix);
+    free(h_vector);
     return 0;
 }
