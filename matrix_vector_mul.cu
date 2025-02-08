3024323c3abf82dae39a34628ed38265050a0bb3
Kelvin <kelvinkonoor@gmail.com>
2025-02-08 13:08:58 +0530

    slight formatting changes

--- matrix_vector_mul.cu
+++ matrix_vector_mul.cu
@@ -16,7 +16,7 @@ __global__ void matrix_vector_mult(int *matrix, int *vector, int *output, size_t
 
 int main() {
     srand(time(NULL));
-    int N = 2;
+    int N = 256;
     int *h_matrix, *h_vector, *h_output, *d_matrix, *d_vector, *d_output;
     size_t matrix_size = sizeof(int) * N * N;
     size_t vector_size = sizeof(int) * N;
@@ -28,7 +28,7 @@ int main() {
     for(int i = 0; i < N * N; i++) 
         h_matrix[i] = rand() % 10;
     
-    for(int i = 0;i < N; i++)
+    for(int i = 0; i < N; i++)
         h_vector[i] = rand() % 10;
 
     cudaMalloc((void**)&d_matrix, matrix_size);
@@ -38,7 +38,7 @@ int main() {
     cudaMemcpy(d_matrix, h_matrix, matrix_size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_vector, h_vector, vector_size, cudaMemcpyHostToDevice);
 
-    dim3 block_size(2);
+    dim3 block_size(64);
     dim3 grid_size((N + block_size.x - 1) / block_size.x);
 
     matrix_vector_mult<<<grid_size, block_size>>>(d_matrix, d_vector, d_output, N);
@@ -46,14 +46,17 @@ int main() {
 
     cudaMemcpy(h_output, d_output, vector_size, cudaMemcpyDeviceToHost);
 
-    for(int j = 0; j < N * N; j++)
-        printf("%d\n", h_matrix[j]);
-
+    for(int j = 0; j < N * N; j++) {
+        if (j % N == 0)
+            printf("\n");
+        printf("%d ", h_matrix[j]);
+    }
+    
     for(int j = 0; j < N; j++)
-        printf("%d\n", h_vector[j]);
+        printf("\n%d", h_vector[j]);
     
     for(int i = 0; i < N; i++)
-        printf("%d\n", h_output[i]);
+        printf("\n%d", h_output[i]);
     
     free(h_matrix);
     free(h_vector);
