ff311d80012535ba7151bfbb37d7d306228e9a34
Kelvin <kelvinkonoor@gmail.com>
2025-02-12 22:21:34 +0530

    random init of host matrices

--- matrix_mult.cu
+++ matrix_mult.cu
@@ -3,6 +3,11 @@
 #include<stdlib.h>
 #include<cuda_runtime.h>
 
+void random_init(float *matrix, int M, int N) {
+    for(int i = 0; i < M * N; i++)
+        matrix[i] = rand() % 10;
+}
+
 int main() {
     float *h_matA, *h_matB, *h_matC, *d_matA, *d_matB, *d_matC;
     int M = 2, N = 2, K = 2;
@@ -13,7 +18,11 @@ int main() {
     h_matA = (float*)malloc(matAsize);
     h_matB = (float*)malloc(matBsize);
     h_matC = (float*)malloc(matCsize);
-    
+
+    random_init(h_matA, M, K);
+    random_init(h_matB, K, N);
+    for(int i = 0; i < M * K; i++) 
+        printf("\n%f", h_matA[i]);
 
     return 0;
 }
