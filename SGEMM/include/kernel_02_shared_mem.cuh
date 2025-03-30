798fc9c4dd724d547eae1769f68baea46efd2b16
Kelvin <kelvinkonoor@gmail.com>
2025-03-30 11:40:39 +0530

    adding function run_sgemm_shared_memory

--- SGEMM/include/kernel_02_shared_mem.cuh
+++ SGEMM/include/kernel_02_shared_mem.cuh
@@ -45,6 +45,36 @@ __global__ void shared_memory(float *matA,
         
     matC[trow * K + tcol] = alpha * sum + beta * matC[trow * K + tcol];
 
-    }
+}
+
+float run_sgemm_shared_memory(float *matA, 
+    float *matB, 
+    float *matC, 
+    float *matD,
+    int M, 
+    int N, 
+    int K,
+    float alpha = 1.0,
+    float beta = 0.0) {
+dim3 block_size(32 * 32);
+dim3 grid_size((K + block_size.x - 1) / block_size.x, (M + block_size.y - 1) / block_size.y);
+cudaEvent_t start, stop;
+float sec = 0.0;
+
+cudaEventCreate(&start);
+cudaEventCreate(&stop);
+
+cudaEventRecord(start);
+shared_memory<<<grid_size, block_size>>>(matA, matB, matC, matD, M, N, K, 1, 0);
+cudaEventRecord(stop);
+cudaEventRecord(stop);
+cudaEventSynchronize(stop);
+cudaEventElapsedTime(&sec, start, stop);
+
+cudaEventDestroy(start);
+cudaEventDestroy(stop);
+
+return sec;
+}
 
 #endif
