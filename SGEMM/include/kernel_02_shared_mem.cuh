b98771a496fc250caf792f521a665023b723f325
Kelvin <kelvinkonoor@gmail.com>
2025-03-30 11:13:05 +0530

    it should be kernel 02

---
+++ SGEMM/include/kernel_02_shared_mem.cuh
@@ -0,0 +1,8 @@
+#ifndef KERNEL_02_SHAREDMEM
+#define KERNEL_02_SHAREDMEM
+#define WRAPSIZE 32
+#include<cuda_runtime.h>
+
+
+
+#endif
--- SGEMM/include/kernel_03_shared_mem.cuh
+++
@@ -1,6 +0,0 @@
-#ifndef KERNEL_03_SHAREDMEM
-#define KERNEL_03_SHAREDMEM
-#define WRAPSIZE 32
-#include<cuda_runtime.h>
-
-#endif
