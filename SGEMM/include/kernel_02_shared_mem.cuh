228c96435f68070538f37f458a2c3f06170c0e74
Kelvin <kelvinkonoor@gmail.com>
2025-03-30 11:17:33 +0530

    sharedA and sharedB will hold the 32*32 block in shared memory

--- SGEMM/include/kernel_02_shared_mem.cuh
+++ SGEMM/include/kernel_02_shared_mem.cuh
@@ -3,6 +3,22 @@
 #define WRAPSIZE 32
 #include<cuda_runtime.h>
 
+// MxN @ NxK + NxK
+__global__ void shared_memory(float *matA, 
+                            float *matB, 
+                            float *matC, 
+                            float *matD,
+                            int M, 
+                            int N, 
+                            int K,
+                            float alpha,
+                            float beta) {
 
+    __shared__ float sharedA[WRAPSIZE * WRAPSIZE];
+    __shared__ float sharedB[WRAPSIZE * WRAPSIZE];
+
+    
+
+    }
 
 #endif
