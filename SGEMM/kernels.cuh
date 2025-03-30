7cd5812d64dfac2fa546682dad1855fd70166dae
Kelvin <kelvinkonoor@gmail.com>
2025-03-30 11:41:19 +0530

    the kernel name should be kernel_02_shared_mem, typo fixed

--- SGEMM/kernels.cuh
+++ SGEMM/kernels.cuh
@@ -3,6 +3,6 @@
 
 #include "include/kernel_00_naive.cuh"
 #include "include/kernel_01_coalesced.cuh"
-#include "include/kernel_03_shared_mem.cuh"
+#include "include/kernel_02_shared_mem.cuh"
 
 #endif
