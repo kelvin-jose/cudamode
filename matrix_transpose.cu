180a98cd67d37f4cae199f55d5a79839e541781e
Kelvin <kelvinkonoor@gmail.com>
2025-05-05 20:42:56 +0530

    random initialization

--- matrix_transpose.cu
+++ matrix_transpose.cu
@@ -4,6 +4,11 @@
 #include<stdlib.h>
 #include<cuda_runtime.h>
 
+void random_init(float *array, size_t size) {
+    for(int i = 0; i < size; i++)
+        array[i] = (float)rand() / RAND_MAX;
+}
+
 int main() {
 
     const int M = 4096, N = 4096;
@@ -13,6 +18,10 @@ int main() {
 
     h_A = (float*)malloc(size);
     h_B = (float*)malloc(size);
+    
+    for(int i = 0; i < size; i++)
+        h_A[i] = (float)rand() / RAND_MAX;
+
     
     return 0;
 }
